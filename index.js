9b9068501970c376b269bc4b12de7021c85896b0
Akil313 <akilhosang@gmail.com>
2019-05-26 14:04:29 -0400

    Sample CRUD operations

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
 const express = require('express');
 const app = express();
+const { check, validationResult } = require('express-validator/check');
+
+app.use(express.json());
+
+const courses = [
+    { id: 1, name: 'course1'},
+    { id: 2, name: 'course2'},
+    { id: 3, name: 'course3'},
+];
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3, 4]);
+    res.send(courses);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    
+    if (!course) res.status(404).send('The course with the given id was not found') //404
+    res.send(course);
+});
+
+app.post('/api/courses', [check('name').isLength({min: 3})], (req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        //400 Bad Request
+        //res.status(422).send('Name is required and should be minimum 3 characters');
+        return res.status(422).json({ errors: errors.array() });
+    }
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+
+    courses.push(course);
+    res.send(course);
 });
 
 //PORT
